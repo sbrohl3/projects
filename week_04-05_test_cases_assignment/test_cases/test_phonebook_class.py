c91a554225d4e11d3941936b302cadd5fdabb30f
Steven Brohl <sbrohl2@academic.walshcollege.edu>
2019-07-29 05:18:27 -0400

    Fixed some commenting; Sending to Professor Petz for a final check

--- week_04-05_test_cases_assignment/test_cases/test_phonebook_class.py
+++ week_04-05_test_cases_assignment/test_cases/test_phonebook_class.py
@@ -5,39 +5,39 @@ class TestPhoneBookClass(unittest.TestCase):
     """Test the PhoneBook Class"""
 
     def setUp(self):
-        """Create an instance of the Pizza class for testing all class functions"""
+        """An instance of the Phonebook class for testing all the other class functions"""
         self.phone_book = Phonebook()
 
     def test_valid_first_name(self):
-        """Here are some name checks I know will validate"""
+        """Here are some name checks I know will validate to True"""
         valid_names_to_test = ["Steven", "steven", "STEVEN", "StEvEn", "sTeVeN", "STEven"]
         
         for name in valid_names_to_test:
             self.assertTrue(self.phone_book.validateNamePart(name))
 
     def test_invalid_first_name(self):
-        """Here are some name checks I know will not validate"""
+        """Here are some name checks I know will not validate to True"""
         invalid_names_to_test = ["123Steven", " steven", "!@11STEVEN", " StEvEn123", "sTeVeN 123", "STEven!234"]
         
         for name in invalid_names_to_test:
             self.assertFalse(self.phone_book.validateNamePart(name))
 
     def test_valid_last_name(self):
-        """Here are some last name checks I know will validate"""
+        """Here are some last name checks I know will validate to True"""
         valid_names_to_test = ["Brohl", "brohl", "BROHL", "BrOhL", "bROhL", "BROhl"]
         
         for name in valid_names_to_test:
             self.assertTrue(self.phone_book.validateNamePart(name))
 
     def test_invalid_last_name(self):
-        """Here are some last name checks I know will not validate"""
+        """Here are some last name checks I know will not validate to True"""
         invalid_names_to_test = ["Brohl ", " brohl", " BROHL ", "123BrOhL", "12!bROhL", "#!@123BROhl"]
         
         for name in invalid_names_to_test:
             self.assertFalse(self.phone_book.validateNamePart(name))
 
     def test_valid_phone_number(self):
-        """Here are some phone number checks I know will validate"""
+        """Here are some phone number checks I know will validate to True"""
         valid_numbers_to_test = ["5864431259", "5867712848", "5861234567", "8007511563", "5551432565", "3135256525"]
         
         for number in valid_numbers_to_test:
@@ -45,7 +45,7 @@ class TestPhoneBookClass(unittest.TestCase):
             self.assertTrue(self.phone_book.validatePhoneNumber())
 
     def test_invalid_phone_number(self):
-        """Here are some phone number checks I know will not validate"""
+        """Here are some phone number checks I know will not validate to True"""
         invalid_numbers_to_test = ["586", "586771", "586abcde", "abcde123456", "5551432565856", "@31352565251aa!"]
         
         for number in invalid_numbers_to_test:
@@ -53,7 +53,7 @@ class TestPhoneBookClass(unittest.TestCase):
             self.assertFalse(self.phone_book.validatePhoneNumber())
 
     def test_valid_phone_number_type(self):
-        """Here are some phone number type checks I know will validate"""
+        """Here are some phone number type checks I know will match a value in valid_phone_number_types"""
         valid_number_types_to_test = ["cell", "home", "office"]
         
         for number_type in valid_number_types_to_test:
@@ -62,30 +62,30 @@ class TestPhoneBookClass(unittest.TestCase):
             self.assertIn(number_type, self.phone_book.valid_phone_number_types)
             
     def test_valid_phone_number_type2(self):
-        """Here are some phone number type checks I know will validate"""
+        """Here are some phone number type checks I know will validate to True"""
         valid_number_types_to_test = ["cell", "home", "office"]
         
         for number_type in valid_number_types_to_test:
             self.phone_book.phone_number_type = number_type
-            ## Checks to see if the phone type matches one in the valid list and evaluates to True
+            ## Checks to see if the phone type function evaluates to True
             self.assertTrue(self.phone_book.validatePhoneNumberType())
             
     def test_invalid_phone_number_type(self):
-        """Here are some phone number type checks I know will not validate"""
+        """Here are some phone number type checks I know will not match a value in valid_phone_number_types"""
         valid_number_types_to_test = ["cell1", "home2", "office ", "cell ", " home", " office", "123cell", "123home", "123office"]
         
         for number_type in valid_number_types_to_test:
             self.phone_book.phone_number_type = number_type
-            ## Checks to see if the phone type does not matche one in the valid list
+            ## Checks to see if the phone type does not match one in the valid list
             self.assertNotIn(number_type, self.phone_book.valid_phone_number_types)
 
     def test_invalid_phone_number_type2(self):
-        """Here are some phone number type checks I know will not validate"""
+        """Here are some phone number type checks I know will not validate to True"""
         valid_number_types_to_test = ["cell1", "home2", "office ", "cell ", " home", " office", "123cell", "123home", "123office"]
         
         for number_type in valid_number_types_to_test:
             self.phone_book.phone_number_type = number_type
-            ## Checks to see if the phone type does not matche one in the valid list and evaluates to False
+            ## Checks to see if the phone type function evaluates to false
             self.assertFalse(self.phone_book.validatePhoneNumberType())
             
             
